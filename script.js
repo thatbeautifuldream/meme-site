4fbc827bdf432dafb52fb0dd7492df6cf142a875
Milind Mishra <milind.mishra4@gmail.com>
2021-07-02 00:24:05 +0530

    update

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function loadMemes() {
     .then((res) => res.json())
     .then((data) => (document.getElementById("meme").src = data.preview[2]));
 }
+function saveMeme() {
+  fetch("https://meme-api.herokuapp.com/gimme")
+    .then((res) => res.json())
+    .then(
+      (data) => (document.getElementById("save-btn").href = data.preview[2])
+    );
+}

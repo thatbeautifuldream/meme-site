414b205efbf6998f763f1fc0e350b4310c7e9814
Milind Mishra <milind.mishra4@gmail.com>
2021-07-11 20:46:25 +0530

    📦 NEW: download button to save the meme

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ function loadMemes() {
     .then((res) => res.json())
     .then((data) => (document.getElementById("meme").src = data.preview[2]));
 }
-function saveMeme() {
-  fetch("https://meme-api.herokuapp.com/gimme")
-    .then((res) => res.json())
-    .then(
-      (data) => (document.getElementById("save-btn").href = data.preview[2])
-    );
+
+let btnDownload = document.getElementById("save-btn");
+let img = document.getElementById("meme");
+
+btnDownload.addEventListener("click", () => {
+  let imagePath = img.getAttribute("src");
+  let fileName = "meme";
+  saveAs(imagePath, fileName);
+});
+
+function getFileName(str) {
+  return str.substring(str.lastIndexOf("/") + 1);
 }

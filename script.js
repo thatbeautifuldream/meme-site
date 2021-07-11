03f0cfc4dc2b6994ebff89e6ae2d9102fe9d1a3c
Milind Mishra <milind.mishra4@gmail.com>
2021-07-11 20:59:51 +0530

    🐛 FIX: actual file name

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ let img = document.getElementById("meme");
 
 btnDownload.addEventListener("click", () => {
   let imagePath = img.getAttribute("src");
-  let fileName = "meme";
+  let fileName = getFileName(imagePath);
   saveAs(imagePath, fileName);
 });
-
+// Dummy URL : https://preview.redd.it/xmtny5krcea71.jpg?width=320&crop=smart&auto=webp&s=20875448c6d34a1985f78f0602a2d32450ab0b9f
 function getFileName(str) {
-  return str.substring(str.lastIndexOf("/") + 1);
+  return str.substring(str.lastIndexOf("/") + 1, str.lastIndexOf("?"));
 }
